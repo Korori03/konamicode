70cf7d0b28ea7707230aea85ad661ce7fa3ddf7e
Ami <31442457+Korori03@users.noreply.github.com>
2019-06-27 09:01:25 -0500

    Create konami.js

---
+++ konami.js
@@ -0,0 +1,21 @@
+/*
+	jQuery Konami Code v1.0.0  2013-05-30
+	(c) 2013 Korori - korori-gaming.com
+	license: http://www.opensource.org/licenses/mit-license.php
+*/
+
+(function($){	
+	$.fn.konami = function(code,callback) {
+		if(code == undefined) code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65,13];
+		var konami_index = 0;
+		$(document).keydown(function(e){
+			if(e.keyCode === code[konami_index++]){
+				if(konami_index === code.length){
+					$(document).unbind('keydown', arguments.callee);
+					if(callback) callback(true);
+				}
+			}else
+				konami_index = 0;
+		});
+	};
+}(jQuery));
